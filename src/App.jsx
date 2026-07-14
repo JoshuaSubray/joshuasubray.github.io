cb611c0075f1c070caa6c739653b3c2da5ebcf61
JoshuaSubray <subrayjoshua@gmail.com>
2026-07-14 17:40:13 -0400

    Added Bootstrap styling.

--- src/App.jsx
+++ src/App.jsx
@@ -2,20 +2,22 @@ import './App.css'
 
 function App() {
   return (
-    <main className="page">
-      <h1>Joshua Subray</h1>
+    <main className="container mt-5">
+      <h1 className="mb-4">Joshua Subray</h1>
 
-      <nav className="navbar">
-        <a href="https://github.com/JoshuaSubray" target="_blank" rel="noreferrer">GitHub</a>
-        <a href="https://linkedin.com/in/joshuasubray" target="_blank" rel="noreferrer">LinkedIn</a>
-        <div className="dropdown">
-          <a href="#projects" className="dropbtn">Projects</a>
-          <div className="dropdown-content">
-            <a href="https://github.com/JoshuaSubray/Currency-Exchange-Concept" target="_blank" rel="noreferrer">Currency Exchange App Concept</a>
-            <a href="https://github.com/JoshuaSubray/Car-Wash-Concept" target="_blank" rel="noreferrer">Car Wash Exchange Website Concept</a>
-            <a href="https://github.com/JoshuaSubray/Pype" target="_blank" rel="noreferrer">Pype: Instant Messaging Website</a>
-            <a href="https://github.com/JoshuaSubray/SeeSharpReviews" target="_blank" rel="noreferrer">See Sharp Reviews: Movie Review Website Concept</a>
-          </div>
+      <nav className="nav mb-4">
+        <a className="nav-link px-0 me-4" href="https://github.com/JoshuaSubray" target="_blank" rel="noreferrer">GitHub</a>
+        <a className="nav-link px-0 me-4" href="https://linkedin.com/in/joshuasubray" target="_blank" rel="noreferrer">LinkedIn</a>
+        <div className="nav-item dropdown">
+          <a className="nav-link dropdown-toggle px-0" href="#projects" id="projectsDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Projects
+          </a>
+          <ul className="dropdown-menu" aria-labelledby="projectsDropdown">
+            <li><a className="dropdown-item" href="https://github.com/JoshuaSubray/Currency-Exchange-Concept" target="_blank" rel="noreferrer">Currency Exchange App Concept</a></li>
+            <li><a className="dropdown-item" href="https://github.com/JoshuaSubray/Car-Wash-Concept" target="_blank" rel="noreferrer">Car Wash Exchange Website Concept</a></li>
+            <li><a className="dropdown-item" href="https://github.com/JoshuaSubray/Pype" target="_blank" rel="noreferrer">Pype: Instant Messaging Website</a></li>
+            <li><a className="dropdown-item" href="https://github.com/JoshuaSubray/SeeSharpReviews" target="_blank" rel="noreferrer">See Sharp Reviews: Movie Review Website Concept</a></li>
+          </ul>
         </div>
       </nav>
 
